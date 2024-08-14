c74d5aa0cbe73a12f6a4045ab8865a7041103734
Todd Garrison <toddlgarrison@gmail.com>
2024-08-14 15:53:45 -0400

    Debugging with print practice

--- dayTwo.py
+++ dayTwo.py
@@ -7,4 +7,15 @@
 # print("3. Add 3g of Salt.")
 # print("4. Leave to rise for 2 hours.")
 # print("5. Bake at 200 degrees C for 30 minutes.")
-print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n 2. Knead the dough for 10 minutes.\n 3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
+
+# print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n 2. Knead the dough for 10 minutes.\n 3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
+
+print("Hello " + "Todd")
+
+# Fix the code below 👇
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
