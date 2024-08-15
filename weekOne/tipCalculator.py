7ad74b18dd5698eef937de70c0795429dfc0c676
Todd Garrison <toddlgarrison@gmail.com>
2024-08-15 16:58:05 -0400

    f string

--- weekOne/tipCalculator.py
+++ weekOne/tipCalculator.py
@@ -4,4 +4,4 @@ tipPercent = int(input("How much of a tip would you like to give? 15, 20, 25?\n"
 numOfPeople = int(input("How many people are splitting the bill?\n"))
 tipAmount = float(tipPercent / 100)
 amountToPay = round(((totalBill + tipAmount)/ numOfPeople), 2)
-print("Each person should pay " + str(amountToPay))
+print(f"Each person should pay {amountToPay}")
