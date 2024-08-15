eec19e0daf50f5c681a927875e26cd6ba0541634
Todd Garrison <toddlgarrison@gmail.com>
2024-08-15 16:50:50 -0400

    bmi Calculator

---
+++ weekOne/bmiCalc.py
@@ -0,0 +1,6 @@
+height = 1.85
+weight = 90
+
+bmi = weight / height ** 2
+
+print(round(bmi))
--- weekOne/dayThree.py
+++ weekOne/dayThree.py
@@ -1,2 +1,9 @@
 #subscripting
 print('hello'[-1])
+
+print(type(123))
+print(type(True))
+print(type('hello'))
+print(type(12.3124))
+
+print("Number of letters in your name: " + str(len(input("Enter your name\n"))))
