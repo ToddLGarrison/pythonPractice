35b5721d0f74f37fa91b35999de405b77e52cfea
Todd Garrison <toddlgarrison@gmail.com>
2024-08-15 15:39:33 -0400

    Variables and len()

--- dayTwo.py
+++ dayTwo.py
@@ -10,7 +10,7 @@
 
 # print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n 2. Knead the dough for 10 minutes.\n 3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
 
-print("Hello " + "Todd")
+# print("Hello " + "Todd")
 
 # Fix the code below 👇
 
@@ -22,4 +22,7 @@ print("Hello " + "Todd")
 
 # input("What is your name")
 
-print("Hello " + input("What is your name "))
+# print("Hello " + input("What is your name ") + "!")
+
+name = input("What is your name? ")
+print(len(("Hello " + name)))
