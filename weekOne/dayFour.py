e1e27bddc0d9c36303d0360ff4803e887748e2ca
Todd Garrison <toddlgarrison@gmail.com>
2024-08-16 15:42:37 -0400

    if/else practice

--- weekOne/dayFour.py
+++ weekOne/dayFour.py
@@ -1,6 +1,6 @@
 num_to_check = int(input("What number do you want to check?\n"))
 
-if(num_to_check % 2 == 0):
+if num_to_check % 2 == 0:
     print(f'{num_to_check} is an even number')
 else:
-    print(f"{num_to_check} is aOdd number")
+    print(f"{num_to_check} is an odd number")
