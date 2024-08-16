fdacdc6011603ff22882c005935245530a7ec045
Todd Garrison <toddlgarrison@gmail.com>
2024-08-16 15:45:02 -0400

    nested if/else practice

--- weekOne/rollerCoasterApp.py
+++ weekOne/rollerCoasterApp.py
@@ -1,7 +1,12 @@
 print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm?"))
+height = int(input("What is your height in cm? "))
 
 if height >= 120:
     print("You can ride the rollercoaster")
+    age = int(input("What is your age? "))
+    if age >= 10:
+        print("The ticket price is $10")
+    else:
+        print("The ticket price is $8")
 else:
     print("Sorry, you are too short and can't ride the rollercoaster")
