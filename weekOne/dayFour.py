dbf4cf99432fc72ad48d2dd68a7087ebe6dcc964
Todd Garrison <toddlgarrison@gmail.com>
2024-08-16 15:37:23 -0400

    Odd number check

---
+++ weekOne/dayFour.py
@@ -0,0 +1,6 @@
+num_to_check = int(input("What number do you want to check?\n"))
+
+if(num_to_check % 2 == 0):
+    print(f'{num_to_check} is an even number')
+else:
+    print(f"{num_to_check} is aOdd number")
