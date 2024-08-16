1ea60e503c092c9a351d73bbd5f6aae4a39c3b44
Todd Garrison <toddlgarrison@gmail.com>
2024-08-16 16:24:02 -0400

    elif practice

--- weekOne/rollerCoasterApp.py
+++ weekOne/rollerCoasterApp.py
@@ -1,12 +1,26 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
-    if age >= 10:
-        print("The ticket price is $10")
+    if age <= 12:
+        bill = 5
+        print("Child ticket price is $5")
+    elif age <= 18:
+        bill = 7
+        print("Youth ticket price is $7")
     else:
-        print("The ticket price is $8")
+        bill = 10
+        print("Adult ticket price is $10")
+
+    want_ticket = input("Do you want a ticket for an added $3? Enter y for yes or n for no ")
+
+    if want_ticket == "y":
+        bill += 3
+        print(f"Your bill is ${bill}")
+    else:
+        print(f"Your bill is ${bill}")
 else:
     print("Sorry, you are too short and can't ride the rollercoaster")
