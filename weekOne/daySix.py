19ce9939b9876a44154677185908e351568a1e06
Todd Garrison <toddlgarrison@gmail.com>
2024-08-21 18:12:46 -0400

    range() practice

--- weekOne/daySix.py
+++ weekOne/daySix.py
@@ -1,6 +1,25 @@
-fruits = ["cherry", "apple", "pineapple"]
-veggies = ["carrot", "cucumber", "onion"]
+# fruits = ["cherry", "apple", "pineapple"]
+# veggies = ["carrot", "cucumber", "onion"]
 
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " pie")
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " pie")
+
+
+student_scores = [12,56,42,58,135,2361,32,12321]
+# sum = 0
+# for num in student_scores:
+#     print(f"number is {num}")
+#     sum += num
+#     print(sum)
+
+max_num = 0
+# for num in student_scores:
+#     if num > max_num:
+#         max_num = num
+#         print(f"max number is {max_num}")
+
+for num in range(0, 101):
+    max_num += num
+
+print(f"max number is {max_num}")
