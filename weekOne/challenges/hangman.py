283a24f75fa12dd66a882835464acdd09c6b32b5
Todd Garrison <toddlgarrison@gmail.com>
2024-08-23 17:41:42 -0400

    Hangman improve UX

--- weekOne/challenges/hangman.py
+++ weekOne/challenges/hangman.py
@@ -2,6 +2,7 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
+lives = 6
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
@@ -9,16 +10,18 @@ placeholder = ""
 
 for letter in chosen_word:
     placeholder += "_"
-print(placeholder)
-
-correct_letters = []
+print("Word to guess: " + placeholder)
 
 game_over = False
-
-lives = 6
+correct_letters = []
 
 while not game_over:
+
+    print(f"***************************{lives}/6 LIVES LEFT***************************")
     guess = input("Please guess a letter: \n").lower()
+
+    if guess in correct_letters:
+        print(f"Already guessed the letter: {guess}")
 
     display = ""
 
@@ -31,16 +34,19 @@ while not game_over:
         else:
             display += "_"
             
+    print("Word to guess: " + display)
 
 
-if guess not in chosen_word:
-    lives -= 1
-    if lives == 0:
-        game_over = True
-        print("You lose!")
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"Letter {guess} is not in the word")
 
-    print(display)
+        if lives == 0:
+            game_over = True
+            print("***************************YOU LOSE!***************************")
 
-    if "_" not in display:
-        print("You win!")
-        game_over = True
+        print(display)
+
+        if "_" not in display:
+            print("***************************YOU WIN!***************************")
+            game_over = True
