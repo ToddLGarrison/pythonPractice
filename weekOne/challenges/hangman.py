ec569886ba14d1f64e3599fe919ca120c603aedf
Todd Garrison <toddlgarrison@gmail.com>
2024-08-23 17:04:14 -0400

    hangman tracking letter changes

--- weekOne/challenges/hangman.py
+++ weekOne/challenges/hangman.py
@@ -11,15 +11,26 @@ for letter in chosen_word:
     placeholder += "_"
 print(placeholder)
 
-guess = input("Please guess a letter: \n").lower()
-print(guess)
+correct_letters = []
 
-display = ""
+game_over = False
 
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
+while not game_over:
+    guess = input("Please guess a letter: \n").lower()
 
-print(display)
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if "_" not in display:
+        print("You win!")
+        game_over = True
