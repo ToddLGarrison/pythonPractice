394720ee7b475a30c8dbad8da563871513a2ceda
Todd Garrison <toddlgarrison@gmail.com>
2024-08-23 16:17:33 -0400

    hangman create placeholder

--- weekOne/challenges/hangman.py
+++ weekOne/challenges/hangman.py
@@ -2,19 +2,25 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
-#randomly select work
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
+placeholder = ""
 
-#ask user to guess a letter, assign as variable and make lowercase
+for letter in chosen_word:
+    placeholder += "_"
+print(placeholder)
+
 guess = input("Please guess a letter: \n").lower()
 print(guess)
-
-#check if letter guessed is one of the letters in chosen word
 
 for letter in chosen_word:
     if letter == guess:
         print("right")
     else:
         print("wrong")
+
+#create placeholder with same num blanks as letters in chosen word
+
+#display the puts guess letter in right spot
+
