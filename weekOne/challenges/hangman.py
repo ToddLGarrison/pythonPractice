1aeda92f0de60dd7cd5a77bbe9557d300a362b4b
Todd Garrison <toddlgarrison@gmail.com>
2024-08-23 15:59:28 -0400

    Hangman select random word

---
+++ weekOne/challenges/hangman.py
@@ -0,0 +1,12 @@
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#randomly select work
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+
+#ask user to guess a letter, assign as variable and make lowercase
+
+#check if letter guessed is one of the letters in chosen word
