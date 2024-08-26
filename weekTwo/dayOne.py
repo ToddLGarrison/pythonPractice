6f89a3606265accf4a1dd980df4406cca0ec5a61
Todd Garrison <toddlgarrison@gmail.com>
2024-08-26 17:12:08 -0400

    nameCalc challenge

--- weekTwo/dayOne.py
+++ weekTwo/dayOne.py
@@ -1,8 +1,16 @@
-name = input("What is your name?")
+name = input("What is your name? \n")
+location = input("What is your location? \n")
 
-def greet():
-    print(f"Hello {name}")
-    print(f"Hello {name}")
-    print(f"Hello {name}")
+# def greet():
+#     print(f"Hello {name}")
+#     print(f"Hello {name}")
+#     print(f"Hello {name}")
 
-greet()
+# greet()
+
+#multi input functions
+def greet_with(x, y):
+    print(f"Hello {x}")
+    print(f"Do you live in {y}?")
+
+greet_with(y = location,x = name)
