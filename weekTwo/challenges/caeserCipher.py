b9975544beee5707e442f67297b013650d30646a
Todd Garrison <toddlgarrison@gmail.com>
2024-09-03 16:40:15 -0400

    Implement index shift functionality

--- weekTwo/challenges/caeserCipher.py
+++ weekTwo/challenges/caeserCipher.py
@@ -7,5 +7,17 @@ shift = int(input("Type the shift number: \n"))
 #Create a function called to encrypt() that takes original_text and shift_amount as 2 inputs
 
 def encrypt(original_text, shift_amount):
+    cipher_text = ""
     print(original_text)
     print(shift_amount)
+
+    for letter in original_text:
+        shifted_index = int(alphabet.index(letter) + shift_amount)
+
+        shifted_index %= len(alphabet)
+        cipher_text += alphabet[shifted_index]
+        
+
+    print(f"Here is your encoded results: {cipher_text}") 
+
+encrypt(text, shift)
