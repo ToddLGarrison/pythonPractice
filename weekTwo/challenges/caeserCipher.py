1b666c36e83b033354138a5097055eabe13dfc27
Todd Garrison <toddlgarrison@gmail.com>
2024-09-03 18:03:51 -0400

    Implement better UX

--- weekTwo/challenges/caeserCipher.py
+++ weekTwo/challenges/caeserCipher.py
@@ -1,27 +1,31 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u' , 'v' , 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decode: \n").lower()
-text = input("Type your message: \n").lower()
-shift = int(input("Type the shift number: \n"))
-
-#Create a function called to encrypt() that takes original_text and shift_amount as 2 inputs
-
-def caesar(original_text, shift_amount):
+def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
-    
-    if direction == 'encode':
-            for letter in original_text:
-                shifted_index = int(alphabet.index(letter) + shift_amount)
+    if encode_or_decode == "decode":
+        shift_amount *= -1
 
-                shifted_index %= len(alphabet)
-                output_text += alphabet[shifted_index]
-    else:
-            for letter in original_text:
-                shifted_index = int(alphabet.index(letter) - shift_amount)
+    for letter in original_text:
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            shifted_index = alphabet.index(letter) + shift_amount
+            shifted_index %= len(alphabet)
+            output_text += alphabet[shifted_index]
+    print(f"Here is your {encode_or_decode}d results: {output_text}") 
 
-                shifted_index %= len(alphabet)
-                output_text += alphabet[shifted_index]
 
-    print(f"Here is your encoded results: {output_text}") 
 
-caesar(text, shift)
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decode: \n").lower()
+    text = input("Type your message: \n").lower()
+    shift = int(input("Type the shift number: \n"))
+
+    caesar(text, shift, direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'. \n").lower()
+    if restart == 'no':
+        should_continue = False
+        print("Goodbye friend!")
