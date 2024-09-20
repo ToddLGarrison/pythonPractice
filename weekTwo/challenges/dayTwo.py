981ab6ac931118f8a2713c00870557af58f27773
Todd Garrison <toddlgarrison@gmail.com>
2024-09-20 10:18:23 -0400

    programming_dictionary

---
+++ weekTwo/challenges/dayTwo.py
@@ -0,0 +1,4 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running.",
+    "Function": "A piece of code taht you can easily call over and over again."
+    }
