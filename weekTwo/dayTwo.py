66bafafcfa1b786571c6af077f185b8995ef2f6c
Todd Garrison <toddlgarrison@gmail.com>
2024-09-23 16:53:53 -0400

    Nested dictionary and lists

--- weekTwo/dayTwo.py
+++ weekTwo/dayTwo.py
@@ -1,32 +1,45 @@
-programming_dictionary = {
-    "Bug": "An error in a program that prevents the program from running.",
-    "Function": "A piece of code that you can easily call over and over again.",
-}
+# programming_dictionary = {
+#     "Bug": "An error in a program that prevents the program from running.",
+#     "Function": "A piece of code that you can easily call over and over again.",
+# }
 
-# print(programming_dictionary)
+# # print(programming_dictionary)
 
-programming_dictionary["Loop"] = "The action of doing something over and over again"
+# programming_dictionary["Loop"] = "The action of doing something over and over again"
 
-# print(programming_dictionary)
+# # print(programming_dictionary)
 
-programming_dictionary["Bug"] = "A moth in the computer"
+# programming_dictionary["Bug"] = "A moth in the computer"
 
-# for key in programming_dictionary:
-#     print(key)
+# # for key in programming_dictionary:
+# #     print(key)
 
 
-capitals = {
-    "France": "Paris",
-    "Germany": "Berlin",
-}
+# capitals = {
+#     "France": "Paris",
+#     "Germany": "Berlin",
+# }
+
+# travel_log = {
+#     "France": ["Paris", "Lille", "Dijon"],
+#     "Germany": ["Berlin", "Stuttgart"],
+# }
+
+# print(travel_log["France"][1])
+
+# nested_list = ["A", "B", ["C", "D"]]
+
+# print(nested_list[2][1])
 
 travel_log = {
-    "France": ["Paris", "Lille", "Dijon"],
-    "Germany": ["Berlin", "Stuttgart"],
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 8,
+        },
+    "Germany": {
+        "cities_visited": ["Berlin", "Stuttgart", "Hamburg"],
+        "total_visits": 3,
+        },
 }
 
-print(travel_log["France"][1])
-
-nested_list = ["A", "B", ["C", "D"]]
-
-print(nested_list[2][1])
+print(travel_log["Germany"]["cities_visited"][2])
