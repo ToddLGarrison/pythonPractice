9c6b8b6d5dee3a96258b7a92e5548779695b7134
Todd Garrison <toddlgarrison@gmail.com>
2024-09-23 16:50:10 -0400

    Nested dictionary

--- weekTwo/dayTwo.py
+++ weekTwo/dayTwo.py
@@ -3,13 +3,30 @@ programming_dictionary = {
     "Function": "A piece of code that you can easily call over and over again.",
 }
 
-print(programming_dictionary)
+# print(programming_dictionary)
 
 programming_dictionary["Loop"] = "The action of doing something over and over again"
 
-print(programming_dictionary)
+# print(programming_dictionary)
 
 programming_dictionary["Bug"] = "A moth in the computer"
 
-for key in programming_dictionary:
-    print(thing)
+# for key in programming_dictionary:
+#     print(key)
+
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Stuttgart"],
+}
+
+print(travel_log["France"][1])
+
+nested_list = ["A", "B", ["C", "D"]]
+
+print(nested_list[2][1])
