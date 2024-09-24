2de10e1709ed59f524d418cbaacd128328e810f5
Todd Garrison <toddlgarrison@gmail.com>
2024-09-24 16:42:40 -0400

    blindAuction completed

--- weekTwo/challenges/blindAuction.py
+++ weekTwo/challenges/blindAuction.py
@@ -1,28 +1,24 @@
-def bid_app():
-    name = input("What is your name? \n")
-    price = float(input("What is your bid $? \n"))
+def find_highest_bid(bidding_dictionary):
+    winner = ""
+    highest_bid = 0
+    
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with the a bid of {highest_bid} and the")
 
-    bids = {}
+bids = {}
+continue_bidding = True
+
+while continue_bidding == True:
+    name = input("What is your name? ")
+    price = float(input("What is your bid $? "))
     bids[name] = price
-
     should_continue = input("Are there any more bidders? Type 'yes' or 'no' \n").lower()
-    
-    continue_bidding = False
-
-    if should_continue == 'yes':
-        continue_bidding = True
-
-    while continue_bidding == True:
-        print("\n" * 20)
-        bid_app()
-    else:
-        for bidder in bids:
-            highest_bidder = {}
-            if(bidder[price] > highest_bidder):
-                highest_bidder = {bidder["name"], bidder["price"]}
-            return highest_bidder
-    
-    print(f"bids", bids)
-
-
-bid_app()
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bid(bids)
+    elif should_continue == "yes":
+        print("\n" * 100)
