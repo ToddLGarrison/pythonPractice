f2ec3ccec795e65706eafa32386eb30d3b2003d9
Todd Garrison <toddlgarrison@gmail.com>
2024-09-24 16:20:27 -0400

    blindAuction update loop

--- weekTwo/challenges/blindAuction.py
+++ weekTwo/challenges/blindAuction.py
@@ -1,27 +1,28 @@
 def bid_app():
-    additional_bidder = False
-    
     name = input("What is your name? \n")
     price = float(input("What is your bid $? \n"))
 
     bids = {}
     bids[name] = price
 
-    more_bidder = input("Are there any more bidders? Type 'yes' or 'no' \n").lower()
+    should_continue = input("Are there any more bidders? Type 'yes' or 'no' \n").lower()
     
-    if(more_bidder == 'yes'):
-        additional_bidder = True
-        if(bidder_dictionary == True):
-            print("\n" * 20)
-            bid_app()
-        else:
-            for bidder in bidder_dictionary:
-                highest_bidder = {}
-                if(bidder[price] > highest_bidder):
-                    highest_bidder = {bidder["name"], bidder["price"]}
-                return highest_bidder
+    continue_bidding = False
+
+    if should_continue == 'yes':
+        continue_bidding = True
+
+    while continue_bidding == True:
+        print("\n" * 20)
+        bid_app()
+    else:
+        for bidder in bids:
+            highest_bidder = {}
+            if(bidder[price] > highest_bidder):
+                highest_bidder = {bidder["name"], bidder["price"]}
+            return highest_bidder
     
-    print(f"bidder dictionary", bidder_dictionary)
+    print(f"bids", bids)
 
 
 bid_app()
