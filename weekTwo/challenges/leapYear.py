bf375bb5ad817aca80b0c927d5ac54850c47bf33
Todd Garrison <toddlgarrison@gmail.com>
2024-09-25 11:54:12 -0400

    docstrings

--- weekTwo/challenges/leapYear.py
+++ weekTwo/challenges/leapYear.py
@@ -1,5 +1,6 @@
 import calendar
 def is_leap_year(year):
+    """Returns if the year is a leap year"""
     # if year % 4 == 0:
     #     if year % 100 == 0:
     #         if year % 400 == 0:
@@ -12,3 +13,5 @@ def is_leap_year(year):
     #     return False
     # return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
     return calendar.isleap(year)
+
+is_leap_year(2018)
