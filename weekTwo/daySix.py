36fde779ba2e900afd694030d0da4e509f7c57da
Todd Garrison <toddlgarrison@gmail.com>
2024-09-27 16:13:00 -0400

    Try Except block

--- weekTwo/daySix.py
+++ weekTwo/daySix.py
@@ -1,4 +1,13 @@
-def my_function():
-    for i in range(1, 21):
-        if i == 20:
-            print("You got it!")
+# def my_function():
+#     for i in range(1, 21):
+#         if i == 20:
+#             print("You got it!")
+
+try:
+    age = int(input("How old is your cat?"))
+except ValueError:
+    print("You have typed in an invalid number, please try again with a numeric value.")
+    age = int(input("How old is your cat?"))
+
+if age > 18:
+    print("Thats an old kitty")
