33a5945a94465d73cd04eabe455678b01531a0ba
Todd Garrison <toddlgarrison@gmail.com>
2024-09-30 16:42:38 -0400

    populate data

--- weekTwo/challenges/highLow/higherLower.py
+++ weekTwo/challenges/highLow/higherLower.py
@@ -1,11 +1,14 @@
 import random
 from hlGameData import data
 
-def game():
-    score = 0
-    more_popular = {}
-    game_continue = True
+# def game():
+#     score = 0
+#     more_popular = {}
+#     game_continue = True
 
-    while (game_continue):
-        
+#     while (game_continue):
 
+
+random_data = random.randint(0,len(data))
+
+print(data[random_data]['name'])
