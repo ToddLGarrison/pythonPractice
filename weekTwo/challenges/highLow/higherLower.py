95ca5e6139efe7a513c1ba2004ac248a3adbd3c9
Todd Garrison <toddlgarrison@gmail.com>
2024-09-30 16:38:54 -0400

    Higher Lower Game set up

---
+++ weekTwo/challenges/highLow/higherLower.py
@@ -0,0 +1,11 @@
+import random
+from hlGameData import data
+
+def game():
+    score = 0
+    more_popular = {}
+    game_continue = True
+
+    while (game_continue):
+        
+
--- weekTwo/daySix.py
+++ weekTwo/daySix.py
@@ -1,13 +1,28 @@
-# def my_function():
-#     for i in range(1, 21):
-#         if i == 20:
-#             print("You got it!")
+# # def my_function():
+# #     for i in range(1, 21):
+# #         if i == 20:
+# #             print("You got it!")
 
-try:
-    age = int(input("How old is your cat?"))
-except ValueError:
-    print("You have typed in an invalid number, please try again with a numeric value.")
-    age = int(input("How old is your cat?"))
+# try:
+#     age = int(input("How old is your cat?"))
+# except ValueError:
+#     print("You have typed in an invalid number, please try again with a numeric value.")
+#     age = int(input("How old is your cat?"))
 
-if age > 18:
-    print("Thats an old kitty")
+# if age > 18:
+#     print("Thats an old kitty")
+
+
+# Target is the number up to which we count
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        if number % 3 == 0:
+            print("Fizz")
+        if number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+
+fizz_buzz(20)
