ccd15abab5c423c35a25b932c9006f3640bd9f49
Todd Garrison <toddlgarrison@gmail.com>
2024-09-30 17:09:50 -0400

    Highlow break down problem

--- weekTwo/challenges/highLow/higherLower.py
+++ weekTwo/challenges/highLow/higherLower.py
@@ -1,14 +1,27 @@
 import random
 from hlGameData import data
 
-# def game():
-#     score = 0
-#     more_popular = {}
-#     game_continue = True
+def game():
+    score = 0
+    more_popular = {}
+    game_continue = True
 
-#     while (game_continue):
+    while (game_continue):
+#Generate random account from data
+        a = random.randint(0,len(data))
+        b = random.randint(0,len(data))
+# display compare data
+        print(f"Compare A: {data[a]['name']}, a {data[a]['description']}, from {data[a]['country']}")
+        print("VS")
+        print(f"Compare B: {data[b]['name']}, a {data[b]['description']}, from {data[b]['country']}")
+# Get answer input
+        answer = print("Who has more followers? 'A' or 'B'").lower()
+# Check if user is correct
+    # get follower count
+    # use if statement to check if user is correct
 
+# give user feedback
 
-random_data = random.randint(0,len(data))
+# keep score
 
-print(data[random_data]['name'])
+# Make repeatable - option b becomes option a if user correct answer
