6eda85c23c3f10bf8eaa26c292c02fc3d3d3b8a6
Todd Garrison <toddlgarrison@gmail.com>
2024-10-02 16:15:36 -0400

    Implement PrettyTable pokemon table

--- weekThree/day16/main.py
+++ weekThree/day16/main.py
@@ -1,13 +1,19 @@
-import prettytable
-from turtle import Turtle, Screen
+from prettytable import PrettyTable
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("cyan2")
-timmy.forward(100)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("cyan2")
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+
+print(table)
