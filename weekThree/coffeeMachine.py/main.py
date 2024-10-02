c47b917e990cb4210a88c4d3bf5e57782debdb8c
Todd Garrison <toddlgarrison@gmail.com>
2024-10-02 16:44:55 -0400

    OOP CM complete

--- weekThree/coffeeMachine.py/main.py
+++ weekThree/coffeeMachine.py/main.py
@@ -1,3 +1,24 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+joe = CoffeeMaker()
+
+johnny = MoneyMachine()
+
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options})")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        joe.report()
+        johnny.report()
+    else:
+        drink = menu.find_drink(choice)
+        if joe.is_resource_sufficient(drink) and johnny.make_payment(drink.cost):
+                joe.make_coffee(drink)
