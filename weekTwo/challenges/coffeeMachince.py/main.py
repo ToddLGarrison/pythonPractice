98adf0f26da6d9955c7f935c5e97a7397d689ae7
Todd Garrison <toddlgarrison@gmail.com>
2024-10-02 10:14:35 -0400

    CM off and report feature

--- weekTwo/challenges/coffeeMachince.py/main.py
+++ weekTwo/challenges/coffeeMachince.py/main.py
@@ -1,12 +1,21 @@
-#3 types of coffee
-#Espresso - $1.50 - 50ml water - 18g coffee
-#Latte - $2.50 - 200ml water - 24g coffee - 150ml Milk
-#Cappuccino - $3.00 - 250ml water - 24g coffee - 150ml Milk
-#Machine - 300ml water - 200ml water - 100g coffee
+from menu import MENU
+from resources import resources
+
 #Coin operated - .01, .05, .10, .25
 
 #print report - what resources are left
 #check resources are sufficient when drink ordered
 #take coins - calculate change - if not enough refund - if correct or more provide change
 #if drink made resources are deducted
+
+coffee_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+machine_on = True
+if coffee_choice == "off":
+    machine_on = False
+    print("Turning machine off...")
+
+
+if coffee_choice == "report":
+    print(resources)
 
