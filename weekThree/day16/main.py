2c0ac52db7923733c942170370bd08bc7a4a9504
Todd Garrison <toddlgarrison@gmail.com>
2024-10-02 16:00:41 -0400

    OOP practice

--- weekThree/day16/main.py
+++ weekThree/day16/main.py
@@ -1,4 +1,11 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 timmy = Turtle()
 print(timmy)
+timmy.shape("turtle")
+timmy.color("cyan2")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
