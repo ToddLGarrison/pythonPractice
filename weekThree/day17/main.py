63d8ba974e588fbfcf28cd411685a3ea7048dbc5
Todd Garrison <toddlgarrison@gmail.com>
2024-10-03 12:37:00 -0400

    Class method practice

--- weekThree/day17/main.py
+++ weekThree/day17/main.py
@@ -3,7 +3,20 @@ class User:
         print("New user being created...")
         self.id = user_id
         self.username = username
+        self.followers = 0
+        self.followings = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.followings += 1
 
 user_1 = User("001", "Todd")
+user_2 = User("002", "Becky")
 
-print(user_1.id, user_1.username)
+print(user_1.id, user_1.username, user_1.followers)
+
+user_1.follow(user_2)
+print(user_1.followings)
+print(user_1.followers)
+print(user_2.followings)
+print(user_2.followers)
