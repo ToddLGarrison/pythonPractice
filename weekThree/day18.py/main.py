4920c91b50fcae259b1accd3fb6643dfe805e959
Todd Garrison <toddlgarrison@gmail.com>
2024-10-17 10:55:27 -0400

    Turtle square function

--- weekThree/day18.py/main.py
+++ weekThree/day18.py/main.py
@@ -1,10 +1,15 @@
 from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("coral")
 
+def square_turtle(x):
+    for i in range(4):
+        timmy_the_turtle.forward(100)
+        timmy_the_turtle.left(90)
 
-
-
+square_turtle(4)
 
 
 
