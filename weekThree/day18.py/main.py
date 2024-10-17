611682497d38cc31852fdb23c7eef6935dcbe42c
Todd Garrison <toddlgarrison@gmail.com>
2024-10-17 11:10:38 -0400

    Dash line square

--- weekThree/day18.py/main.py
+++ weekThree/day18.py/main.py
@@ -4,12 +4,19 @@ timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("coral")
 
-def square_turtle(x):
-    for i in range(4):
-        timmy_the_turtle.forward(100)
+def draw_dash_line(x):
+    for i in range(x):
+        timmy_the_turtle.forward(10)
+        timmy_the_turtle.color("white")
+        timmy_the_turtle.forward(10)
+        timmy_the_turtle.color("coral")
+
+def square_turtle(x,y):
+    for i in range(x):
+        draw_dash_line(y)
         timmy_the_turtle.left(90)
 
-square_turtle(4)
+square_turtle(4,5)
 
 
 
