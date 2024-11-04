86b28c2bb1095bffbc665f43d0d820b40d1ffbcc
Todd Garrison <toddlgarrison@gmail.com>
2024-11-04 16:50:18 -0500

    turtle_race setup complete

--- weekThree/day19/turtle_race/main.py
+++ weekThree/day19/turtle_race/main.py
@@ -4,8 +4,15 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle do you think will win the race?: ")
-print(user_bet)
+colors = ["red", "green", "yellow", "blue", "purple", "orange"]
+y_positions = [-70, -40, -10, 20, 50, 80]
 
-tim = Turtle()
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    time.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(x=-230, y=y_positions[turtle_index])
+
+
 
 screen.exitonclick()
