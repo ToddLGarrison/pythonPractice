a963ef2d068a83918897e2c8ef36b0679c42174b
Todd Garrison <toddlgarrison@gmail.com>
2024-11-05 13:31:22 -0500

    implement move snake functionality

--- weekThree/day19/snake_game/snake.py
+++ weekThree/day19/snake_game/snake.py
@@ -2,12 +2,17 @@ from turtle import Turtle
 
 STARTING_POSITIONS = starting_position = [(0, 0), (-20, 0), (-30, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
 
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -23,4 +28,20 @@ class Snake:
                 new_y = self.segments[seg_num - 1].ycor()
                 self.segments[seg_num].goto(new_x, new_y)
             
-            self.segments[0].forward(MOVE_DISTANCE)
+            self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.heading() !=DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.heading() !=UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.heading() !=RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.heading()!= LEFT:
+            self.head.setheading(RIGHT)
