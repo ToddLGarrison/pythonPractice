59db7b40f82dccfbfaff74a41902435a0f10a777
Todd Garrison <toddlgarrison@gmail.com>
2024-11-18 14:23:11 -0500

    initiate ball and ball movement

--- weekThree/day20/main.py
+++ weekThree/day20/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,6 +10,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -20,6 +22,7 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 while game_is_on:
     screen.update()
+    ball.move()
 
 
 screen.exitonclick()
