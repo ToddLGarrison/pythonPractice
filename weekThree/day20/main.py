f82c480841b44bf4e19882e747cb7745bec52b77
Todd Garrison <toddlgarrison@gmail.com>
2024-11-18 14:45:01 -0500

    Implement ball reset

--- weekThree/day20/main.py
+++ weekThree/day20/main.py
@@ -31,8 +31,13 @@ while game_is_on:
         ball.bounce_y()
 
     # Detect collision with paddles
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    if ball.xcor() > 380:
+        ball.reset_position()
+    
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
