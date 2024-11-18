cf59f7180604557d5d03cb0dcb771f4abfcf11db
Todd Garrison <toddlgarrison@gmail.com>
2024-11-18 14:16:08 -0500

    Initiate paddles

--- weekThree/day20/main.py
+++ weekThree/day20/main.py
@@ -1,8 +1,22 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 screen.exitonclick()
