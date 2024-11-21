41ffc3d85c65627f9937b81fb9903bfd8661651f
Todd Garrison <toddlgarrison@gmail.com>
2024-11-21 16:06:54 -0500

    pandas practice

--- weekFour/day25/main.py
+++ weekFour/day25/main.py
@@ -15,4 +15,12 @@ import pandas
 
 data = pandas.read_csv("weather_data.csv")
 
-print(data)
+# print(data["temp"])
+
+data_dict = data.to_dict()
+# print(data_dict)
+
+temp_list = data["temp"].to_list()
+# print(len(temp_list))
+
+print(sum(temp_list))
