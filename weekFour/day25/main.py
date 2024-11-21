67a30a8420959387200f1fe73086fda9dfefc429
Todd Garrison <toddlgarrison@gmail.com>
2024-11-21 16:26:42 -0500

    Continued Pandas practice

--- weekFour/day25/main.py
+++ weekFour/day25/main.py
@@ -17,10 +17,20 @@ data = pandas.read_csv("weather_data.csv")
 
 # print(data["temp"])
 
-data_dict = data.to_dict()
-# print(data_dict)
+# data_dict = data.to_dict()
+# # print(data_dict)
 
-temp_list = data["temp"].to_list()
-# print(len(temp_list))
+# temp_list = data["temp"].to_list()
+# # print(len(temp_list))
 
-print(sum(temp_list))
+# print(data["temp"].mean())
+# print(data["temp"].max())
+
+# print(data.condition)
+
+# print(data[data.day == "Monday"])
+
+# print(data[data.temp == data.temp.max()])
+monday = data[data.day == "Monday"]
+monday_temp = monday.temp[0] * (9/5) + 32
+print(monday_temp)
