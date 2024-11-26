8f8e668fea72f66f5e143bfe887a064cdfb8c2c8
Todd Garrison <toddlgarrison@gmail.com>
2024-11-26 14:35:58 -0500

    US State game set up

---
+++ weekFour/day26/usMap/main.py
@@ -0,0 +1,14 @@
+import turtle
+import tkinter
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+image = "blank_states_img.gif"
+
+turtle.addshape(image)
+
+
+
+
+screen.exitonclick()
