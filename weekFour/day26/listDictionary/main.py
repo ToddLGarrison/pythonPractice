fc5eb72cd2ce6a9e87d5ad3097a49669fb3250cd
Todd Garrison <toddlgarrison@gmail.com>
2024-11-26 15:19:32 -0500

    Practice list comprehension

--- weekFour/day26/listDictionary/main.py
+++ weekFour/day26/listDictionary/main.py
@@ -1,4 +1,19 @@
-numbers = [1,2,3,4]
+# numbers = [1,2,3,4]
 
-new_list = [n+1 for n in numbers]
-print(new_list)
+# new_list = [n+1 for n in numbers]
+# print(new_list)
+
+# name = "Todd"
+# letter_list = [l for l in name]
+# print(letter_list)
+
+new_nums = [n * 2 for n in range(1,5)]
+print(new_nums)
+
+names = ["Alex", "Beth", 'Caroline', 'Dave', 'Eleanor','Freddie']
+
+short_names = [name for name in names if len(name) <5]
+print(short_names)
+
+long_names = [name.upper() for name in names if len(name) >5]
+print(long_names)
