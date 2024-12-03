ba653b486109b1f2d882c88ea7bef4a992a09316
Todd Garrison <toddlgarrison@gmail.com>
2024-12-03 18:14:02 -0500

    tkinter GUI practice

--- weekFour/day27/main.py
+++ weekFour/day27/main.py
@@ -5,4 +5,9 @@ window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
+
+my_label = tkinter.Label(text="I am a Label", font=("Ariel", 24, "bold"))
+my_label.pack(side=("left"))
+
+
 window.mainloop()
