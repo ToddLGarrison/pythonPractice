5ccd12eadfa16b3ea2028bac377b377db9e263e3
Todd Garrison <toddlgarrison@gmail.com>
2024-12-04 11:06:12 -0500

    tkinter button challenge

--- weekFour/day27/main.py
+++ weekFour/day27/main.py
@@ -1,13 +1,22 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 
-my_label = tkinter.Label(text="I am a Label", font=("Ariel", 24, "bold"))
+my_label = Label(text="I am a Label", font=("Ariel", 24, "bold"))
 my_label.pack(side=("left"))
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+def button_clicked():
+    print("I got clicked")
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
 
 
 window.mainloop()
