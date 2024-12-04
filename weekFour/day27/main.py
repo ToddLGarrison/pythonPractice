4c3e0b18383f2b1b401a4edb24198369d33e176d
Todd Garrison <toddlgarrison@gmail.com>
2024-12-04 11:12:02 -0500

    tkinter input practice

--- weekFour/day27/main.py
+++ weekFour/day27/main.py
@@ -13,10 +13,14 @@ my_label["text"] = "New Text"
 my_label.config(text="New Text")
 
 def button_clicked():
-    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
 
 button = Button(text="Click Me", command=button_clicked)
 button.pack()
 
+
+input = Entry(width=10)
+input.pack()
 
 window.mainloop()
