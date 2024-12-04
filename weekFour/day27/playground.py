0b38d8dae2b94a2e7fb6c1b9e98ce3c44a09795f
Todd Garrison <toddlgarrison@gmail.com>
2024-12-04 10:49:37 -0500

    **kwargs practice

--- weekFour/day27/playground.py
+++ weekFour/day27/playground.py
@@ -5,3 +5,14 @@ def add(*args):
     return print(total)
 
 add(2,3,5,6,1,23,5)
+
+def calculate(n, **kwargs):
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
