e174796835e2427c660fa3135cc2f29b89592796
Todd Garrison <toddlgarrison@gmail.com>
2024-12-04 11:00:35 -0500

    Option Argument tests

--- weekFour/day27/playground.py
+++ weekFour/day27/playground.py
@@ -16,3 +16,16 @@ def calculate(n, **kwargs):
 
 
 calculate(2, add=3, multiply=5)
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw["make"]
+        self.model = kw["model"]
+
+my_car = Car(make="Tesla", model="Y")
+print(my_car.model)
+
+def all_aboard(a, *args, **kw): 
+    print(a, args, kw)
+
+all_aboard(4, 7, 3, 0, x=10, y=64)
