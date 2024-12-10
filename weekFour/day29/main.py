1147ba0632e667801c44bf76c513064e8f950e38
Todd Garrison <toddlgarrison@gmail.com>
2024-12-10 10:59:17 -0500

    Adjust layout

--- weekFour/day29/main.py
+++ weekFour/day29/main.py
@@ -8,7 +8,7 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
 logo_image = PhotoImage(file="logo.png")
@@ -24,11 +24,11 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entry boxes
-website_entry = Entry(width=35)
+website_entry = Entry(width=39)
 website_entry.grid(row=1, column=1, columnspan=2)
-email_entry = Entry(width=35)
+email_entry = Entry(width=39)
 email_entry.grid(row=2, column=1, columnspan=2)
-password_entry = Entry(width=21)
+password_entry = Entry(width=22)
 password_entry.grid(row=3, column=1)
 
 # Buttons
