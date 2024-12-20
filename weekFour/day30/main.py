a5522281e78ffea76b86988fd859c6e5efe892c0
Todd Garrison <toddlgarrison@gmail.com>
2024-12-20 14:11:29 -0500

    Error handling messaging

--- weekFour/day30/main.py
+++ weekFour/day30/main.py
@@ -1,5 +1,9 @@
 try:
     file = open("a_file.text")
-except:
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["adgahwwg"])
+except FileNotFoundError:
     file = open("a_file.text", "w")
     file.write("This is the content of the file.")
+except KeyError as error_message:
+    print(f"The key {error_message} you're trying to access does not exist in the dictionary.")
