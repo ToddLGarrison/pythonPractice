2595de73cbd632e364685b946e03b841a39faa4c
Todd Garrison <toddlgarrison@gmail.com>
2024-12-20 14:13:48 -0500

    else/finally practice

--- weekFour/day30/main.py
+++ weekFour/day30/main.py
@@ -1,9 +1,15 @@
 try:
     file = open("a_file.text")
     a_dictionary = {"key": "value"}
-    print(a_dictionary["adgahwwg"])
+    print(a_dictionary["key"])
 except FileNotFoundError:
     file = open("a_file.text", "w")
     file.write("This is the content of the file.")
 except KeyError as error_message:
     print(f"The key {error_message} you're trying to access does not exist in the dictionary.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("The file has been closed.")
